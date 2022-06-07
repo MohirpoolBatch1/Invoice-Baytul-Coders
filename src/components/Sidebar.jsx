d01e246873cb11a131a3cf6e1c6189b089d1ed5d
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2022-06-07 12:50:08 +0500

    all px changed to rem

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,16 +6,16 @@ import Theme from '../assets/icon-moon.svg'
 
 function Sidebar() {
   return (
-    <div className="sidebar h-screen w-[103px] rounded-tr-[20px] rounded-br-[20px]">
-      <div className="sidebar-top h-[103px] w-[103px] bg-purple absolute rounded-br-[20px] rounded-tr-[20px] flex justify-center items-center">
+    <div className="sidebar h-screen w-[6.438rem] rounded-tr-[1.25rem] rounded-br-[1.25rem]">
+      <div className="sidebar-top h-[6.438rem] w-[6.438rem] bg-purple absolute rounded-br-[1.25rem] rounded-tr-[1.25rem] flex justify-center items-center">
         <img src={Logo} alt="Logo" width={40} height={40} className="z-10" />
-        <div className="sidebar-inner h-[52px] w-[103px] bg-purple-light absolute top-[51px] rounded-tl-[20px] rounded-br-[20px]"></div>
+        <div className="sidebar-inner h-[3.25rem] w-[6.438rem] bg-purple-light absolute top-[3.188rem] rounded-tl-[1.25rem] rounded-br-[1.25rem]"></div>
       </div>
-      <div className="sidebar-bottom w-[103px] bg-[#373B53] h-screen bg-grayBlack rounded-tr-[20px] rounded-br-[20px] flex flex-col justify-end items-center ">
+      <div className="sidebar-bottom w-[6.438rem] bg-[#373B53] h-screen bg-grayBlack rounded-tr-[1.25rem] rounded-br-[1.25rem] flex flex-col justify-end items-center ">
         <div className="sidebar-theme pb-8">
           <img src={Theme} alt="Theme" width={20} height={20} />
         </div>
-        <div className="w-full bg-gray-400 h-[1px] inline-block" />
+        <div className="w-full bg-gray-400 h-[0.063rem] inline-block" />
         <div className="sidebar-avatar py-6">
           <NavLink to={'/'}>
             <img
