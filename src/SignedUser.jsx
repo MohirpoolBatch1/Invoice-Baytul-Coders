cc52ec547ed9e400d45569e6f6556bc65af34229
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2022-06-09 22:05:07 +0500

    initial Invoice-Items

--- src/SignedUser.jsx
+++ src/SignedUser.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Button from './components/Button/Button.jsx'
+// import Button from './components/Button/Button.jsx'
+import ViewInvoice from './pages/View-invoice/ViewInvoice.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import Homepage from './pages/Homepage/Homepage.jsx'
 
 const SignedUser = () => (
   <div className="App font-spartan font-medium h-screen w-screen flex items-center bg-gray-100">
     <Sidebar />
-    <Button buttonKind="danger">New Invoice</Button>
+    {/* <Button buttonKind="danger">New Invoice</Button> */}
+    <ViewInvoice />
     <Routes>
       <Route path="/" element={<Homepage />} />
     </Routes>
