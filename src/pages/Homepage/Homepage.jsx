0902bcc80d3bc6a42503c34ea25a01bd08ee471f
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2022-06-24 09:43:22 +0500

    feat(SSP-36): Kamchiliklar tuzatildi

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {uniqueId} from 'lodash'
 import EmptyPage from './EmptyPage.jsx'
 import InvoiceNavbar from './InvoiceNavbar.jsx'
@@ -21,10 +21,45 @@ const InvoiceItems = props => (
 )
 
 const Homepage = () => {
+  const [amountInvoices, setAmountInvoices] = useState(
+    items.length ? `There are ${items.length} total invoices` : 'No invoices',
+  )
   const [invoiceItems, setInvoiceItems] = useState(items)
+  const [statuses, setStatuses] = useState({
+    paid: false,
+    pending: false,
+    draft: false,
+  })
+
+  const changeHandler = e => {
+    setStatuses({
+      ...statuses,
+      [e.target.name]: e.target.checked,
+    })
+  }
+
+  useEffect(() => {
+    const filteredInvoices = items.filter(item => statuses[item.status])
+    setInvoiceItems(filteredInvoices.length > 0 ? filteredInvoices : items)
+
+    const amount = items.length
+      ? `There are ${items.length} total invoices`
+      : 'No invoices'
+
+    setAmountInvoices(
+      filteredInvoices.length > 0
+        ? `There are ${filteredInvoices.length} pending invoices`
+        : amount,
+    )
+  }, [statuses, amountInvoices])
+
   return (
-    <div className="mx-auto min-w-[45.6rem] px-6">
-      <InvoiceNavbar amountInvoices={invoiceItems.length} />
+    <div className="mx-auto w-full max-w-[45.6rem] px-6">
+      <InvoiceNavbar
+        amountInvoices={amountInvoices}
+        statuses={statuses}
+        changeHandler={changeHandler}
+      />
       {items.length !== 0 ? (
         <InvoiceItems invoiceItems={invoiceItems} />
       ) : (
