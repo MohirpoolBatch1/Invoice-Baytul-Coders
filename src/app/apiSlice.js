677140ae1246354d0e732417f6d2477ddd649807
khumoyun-eddie <eddie.freelance21@gmail.com>
2022-07-29 04:52:56 +0500

    refactor 2

--- src/app/apiSlice.js
+++ src/app/apiSlice.js
@@ -1,15 +1,17 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import {baseUrl} from './constants'
 
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl,
+    baseUrl: 'https://serene-plains-38345.herokuapp.com',
   }),
   endpoints: builder => ({
     getSingleInvoice: builder.query({
       query: ({id}) => `/invoice/${id}`,
     }),
+    getAllInvoices: builder.query({
+      query: () => `/invoice`,
+    }),
   }),
 })
-export const {useGetSingleInvoiceQuery} = apiSlice
+export const {useGetSingleInvoiceQuery, useGetAllInvoicesQuery} = apiSlice
